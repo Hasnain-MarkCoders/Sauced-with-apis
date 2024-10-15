76bbee2cb8e8d1d5a09ecd67fe4d9bf00cd1a68d
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2024-10-15 21:57:19 +0500

    firebase message version number fixed

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,11 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-const config = {
-  resolver: {
-    blacklistRE: exclusionList([
-      /node_modules\/.*\/node_modules\/react-native\/.*/, // Exclude nested react-native modules
-      /android\/.*/,
-      /ios\/.*/,
-      /dist\/.*/,
-      /build\/.*/,
-      /.*\.test\.js$/, // Exclude test files
-      /.*\.spec\.js$/,
-      /.*\.stories\.js$/, // Exclude Storybook files if any
-    ]),
-  },
-  watchFolders: [
-    require('path').resolve(__dirname, 'src'), // Specify only essential folders
-  ],
-  transformer: {
-    // Only include if using react-native-svg-transformer
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-};
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
