47b34af4df98fc7bafa579aae8aa60e7a097960d
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2024-10-29 00:14:51 +0500

    purpose string fix apple auth duplicate function removed

--- src/screens/SocialSignIn/SocialSignIn.jsx
+++ src/screens/SocialSignIn/SocialSignIn.jsx
@@ -101,31 +101,31 @@ const SocialSignIn = () => {
     });
   }, []);
 
-  async function onAppleButtonPress() {
-    //   try{
-    // // Start the sign-in request
-    // const appleAuthRequestResponse = await appleAuth.performRequest({
-    //   requestedOperation: appleAuth.Operation.LOGIN,
-    //   // As per the FAQ of react-native-apple-authentication, the name should come first in the following array.
-    //   // See: https://github.com/invertase/react-native-apple-authentication#faqs
-    //   requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
-    // });
-    // console.log("appleAuthRequestResponse==============>", appleAuthRequestResponse)
-    // // Ensure Apple returned a user identityToken
-    // if (!appleAuthRequestResponse.identityToken) {
-    //   throw new Error('Apple Sign-In failed - no identify token returned');
-    // }
-    // // Create a Firebase credential from the response
-    // const { identityToken, nonce } = appleAuthRequestResponse;
-    // const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
-    // console.log("identityToken============>", identityToken)
-    // // Sign the user in with the credential
-    // return auth().signInWithCredential(appleCredential);
-    //   }catch(err){
-    // console.log(err)
-    //   }finally{
-    //   }
-  }
+  // async function onAppleButtonPress() {
+  //   //   try{
+  //   // // Start the sign-in request
+  //   // const appleAuthRequestResponse = await appleAuth.performRequest({
+  //   //   requestedOperation: appleAuth.Operation.LOGIN,
+  //   //   // As per the FAQ of react-native-apple-authentication, the name should come first in the following array.
+  //   //   // See: https://github.com/invertase/react-native-apple-authentication#faqs
+  //   //   requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+  //   // });
+  //   // console.log("appleAuthRequestResponse==============>", appleAuthRequestResponse)
+  //   // // Ensure Apple returned a user identityToken
+  //   // if (!appleAuthRequestResponse.identityToken) {
+  //   //   throw new Error('Apple Sign-In failed - no identify token returned');
+  //   // }
+  //   // // Create a Firebase credential from the response
+  //   // const { identityToken, nonce } = appleAuthRequestResponse;
+  //   // const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
+  //   // console.log("identityToken============>", identityToken)
+  //   // // Sign the user in with the credential
+  //   // return auth().signInWithCredential(appleCredential);
+  //   //   }catch(err){
+  //   // console.log(err)
+  //   //   }finally{
+  //   //   }
+  // }
 
   const signInWithGoogle = async () => {
     setLoading(true);
