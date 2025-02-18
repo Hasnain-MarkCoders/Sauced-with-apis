4a916916df3da0ad9159b87b3f1238b061720c2e
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2025-02-18 22:41:20 +0500

    android update working done by implementing IAUUpdateKinf.IMMEDIATE

--- App.jsx
+++ App.jsx
@@ -7,8 +7,8 @@ import { LogBox } from 'react-native';
 import BootSplash from "react-native-bootsplash";
 import Toast, { BaseToast }  from 'react-native-toast-message';
 import { scale } from 'react-native-size-matters';
-import { Platform } from 'react-native';
-import { NativeModules } from 'react-native';
+import { Alert, Linking, Platform } from 'react-native';
+import SpInAppUpdates, { IAUUpdateKind } from 'sp-react-native-in-app-updates';
 
 function App() {
 LogBox.ignoreLogs(['Warning: ...']); 
@@ -27,6 +27,45 @@ React.useEffect(() => {
     await BootSplash.hide({ fade: true });
   });
 }, []);
+
+
+React.useEffect(() => {
+  const checkForUpdate = async () => {
+    const inAppUpdates = new SpInAppUpdates(false);  // Disable debug mode for production
+    try {
+      // Check if an update is available without needing to manually specify the version
+      const result = await inAppUpdates.checkNeedsUpdate();
+
+      console.log('Update check result:', result);
+
+      // For Android
+      if (Platform.OS === 'android') {
+        if (result.shouldUpdate) {
+          console.log('Android update available');
+          inAppUpdates.startUpdate({ updateType: IAUUpdateKind.IMMEDIATE });  // Force update immediately
+        } else {
+          console.log('No update needed on Android');
+        }
+      }
+
+      // For iOS
+      else if (Platform.OS === 'ios') {
+        if (result.shouldUpdate) {
+          console.log('iOS update available. Redirecting to App Store.');
+          const appStoreURL = 'itms-apps://apps.apple.com/us/app/sauced-app/id6737129890';  // Your App Store URL
+          Linking.openURL(appStoreURL);  // Redirect to App Store
+        } else {
+          console.log('No update needed for iOS.');
+        }
+      }
+    } catch (error) {
+      console.error('Error checking update:', error);
+    }
+  };
+
+  checkForUpdate();  // Trigger the check on app startup
+}, []);
+
 
 const toastConfig = {
   /*
